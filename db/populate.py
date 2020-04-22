d581b64e44c72f76865733ff11d309865684fb6b
svenskapojkarna <hytonenaleksi@gmail.com>
2020-04-22 14:49:33 +0300

    Removed https:// from article links

--- db/populate.py
+++ db/populate.py
@@ -1,4 +1,4 @@
-from db.db import db, Articles, Users, AddedArticles
+from db import db, Articles, Users, AddedArticles
 from datetime import datetime
 
 db.create_all()
@@ -9,7 +9,7 @@ print('Adding Initial articles to database...')
 
 tmp = Articles(
     date='02.01.2019',
-    link='https://www.wcjb.com/content/news/Florida-man-doesnt-get-straw-attacks-McDonalds-employee-503812581.html', 
+    link='www.wcjb.com/content/news/Florida-man-doesnt-get-straw-attacks-McDonalds-employee-503812581.html', 
     headline="Florida man doesn't get straw, attacks McDonald's employee", 
     modtime=datetime.now()
 )
@@ -18,7 +18,7 @@ db.session.commit()
 
 tmp = Articles(
     date='01.02.2019',
-    link='https://www.orlandoweekly.com/Blogs/archives/2019/02/01/a-man-actually-punted-a-rabid-coyote-in-kissimmee', 
+    link='www.orlandoweekly.com/Blogs/archives/2019/02/01/a-man-actually-punted-a-rabid-coyote-in-kissimmee', 
     headline="A Man Actually Punted a Rabid Coyote in Kissimmee", 
     modtime=datetime.now()
 )
@@ -27,7 +27,7 @@ db.session.commit()
 
 tmp = Articles(
     date='01.03.2019',
-    link='https://www.tampabay.com/florida-politics/buzz/2019/03/01/florida-house-speaker-apologizes-for-referring-to-pregnant-women-as-host-bodies-in-interview-on-abortion/', 
+    link='www.tampabay.com/florida-politics/buzz/2019/03/01/florida-house-speaker-apologizes-for-referring-to-pregnant-women-as-host-bodies-in-interview-on-abortion/', 
     headline="Florida House Speaker Apologizes for Referring to Pregnant Women as ‘Host Bodies’ in Interview on Abortion", 
     modtime=datetime.now()
 )
@@ -36,7 +36,7 @@ db.session.commit()
 
 tmp = Articles(
     date='01.04.2019',
-    link='http://www.brevardtimes.com/2018/04/florida-man-catches-cooks-baby-manatee/', 
+    link='www.brevardtimes.com/2018/04/florida-man-catches-cooks-baby-manatee/', 
     headline="Florida Man Catches, Cooks Baby Manatee", 
     modtime=datetime.now()
 )
@@ -45,7 +45,7 @@ db.session.commit()
 
 tmp = Articles(
     date='01.05.2019',
-    link='https://wtfflorida.com/news/crime/after-crashing-his-car-florida-man-asks-trooper-if-he-can-get-more-meth/', 
+    link='wtfflorida.com/news/crime/after-crashing-his-car-florida-man-asks-trooper-if-he-can-get-more-meth/', 
     headline="After Crashing his Car, Florida Man Asks Trooper if He Can Get More Meth", 
     modtime=datetime.now()
 )
@@ -54,7 +54,7 @@ db.session.commit()
 
 tmp = Articles(
     date='01.06.2018',
-    link='https://wtfflorida.com/news/crime/florida-man-flees-from-car-crash-crashes-twice-again-cusses-out-good-samaritan-victims/', 
+    link='wtfflorida.com/news/crime/florida-man-flees-from-car-crash-crashes-twice-again-cusses-out-good-samaritan-victims/', 
     headline="Florida Man Flees from Car Crash, Crashes Twice Again, Cusses Out Good Samaritan Victims", 
     modtime=datetime.now()
 )
@@ -63,7 +63,7 @@ db.session.commit()
 
 tmp = Articles(
     date='09.07.2019',
-    link='https://www.rblandmark.com/News/Articles/7-9-2019/Florida-man-chants-as-police-try-to-arrest-him-%7C-Police-reports-July-1_7/', 
+    link='www.rblandmark.com/News/Articles/7-9-2019/Florida-man-chants-as-police-try-to-arrest-him-%7C-Police-reports-July-1_7/', 
     headline="Florida man chants as police try to arrest him", 
     modtime=datetime.now()
 )
@@ -72,7 +72,7 @@ db.session.commit()
 
 tmp = Articles(
     date='01.08.2018',
-    link='https://www.country935.ca/2018/08/01/florida-man-carries-live-alligator-liquor-store/', 
+    link='www.country935.ca/2018/08/01/florida-man-carries-live-alligator-liquor-store/', 
     headline="Florida Man carries Live Alligator into Liquor Store", 
     modtime=datetime.now()
 )
@@ -81,7 +81,7 @@ db.session.commit()
 
 tmp = Articles(
     date='01.09.2018',
-    link='https://www.wsbradio.com/news/florida-man-accused-shooting-home-after-woman-leaves-negative-restaurant-review/sPcDDNf3nhs5jIk1GDamwM/', 
+    link='www.wsbradio.com/news/florida-man-accused-shooting-home-after-woman-leaves-negative-restaurant-review/sPcDDNf3nhs5jIk1GDamwM/', 
     headline="Florida man accused of shooting at home after woman leaves negative restaurant review", 
     modtime=datetime.now()
 )
@@ -90,7 +90,7 @@ db.session.commit()
 
 tmp = Articles(
     date='01.10.2019',
-    link='https://www.wwnytv.com/2019/10/01/florida-man-tried-sell-beer-dolphins-game-police-say/', 
+    link='www.wwnytv.com/2019/10/01/florida-man-tried-sell-beer-dolphins-game-police-say/', 
     headline="Florida man tried to sell beer for $724 at Dolphins game, police say", 
     modtime=datetime.now()
 )
@@ -99,7 +99,7 @@ db.session.commit()
 
 tmp = Articles(
     date='13.11.2018',
-    link='https://newschannel9.com/news/offbeat/florida-man-breaks-into-restaurant-strips-naked-eats-noodles-plays-bongos', 
+    link='newschannel9.com/news/offbeat/florida-man-breaks-into-restaurant-strips-naked-eats-noodles-plays-bongos', 
     headline="Florida man breaks into restaurant, strips naked, eats noodles, plays bongos", 
     modtime=datetime.now()
 )
